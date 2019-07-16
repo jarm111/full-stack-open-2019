5332eecd8ec5bbdcb9f8be2ecb33029653b63136
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-07-16 15:50:17 +0300

    Add 1.8: unicafe step3

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,12 +1,28 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = ({stats}) => {
+  const {good, neutral, bad} = stats
+  const all = good + neutral + bad
+
+  return (
+    <>
+      <h1>statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {all === 0 ? 0 : (good * 1 + bad * -1) / all}</div>
+      <div>positive {all === 0 ? 0 : good / all * 100} %</div>
+    </>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = good + neutral + bad
 
   return (
     <div>
@@ -14,13 +30,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>statistics</h1>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {all}</div>
-      <div>average {all === 0 ? 0 : (good * 1 + bad * -1) / all}</div>
-      <div>average {all === 0 ? 0 : good / all * 100} %</div>
+      <Statistics stats={{good, neutral, bad}}/>
     </div>
   )
 }
