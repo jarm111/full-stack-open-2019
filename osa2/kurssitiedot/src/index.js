15f1d2f4008d7bb8ee8a5c55b4b00f85f9afd2a0
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-07-19 15:30:27 +0300

    Add 2.5: erillinen moduuli

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,45 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Header = (props) => (
-  <h2>{props.course.name}</h2>
-)
-
-const Part = (props) => (
-  <p>{props.part} {props.exercises}</p>
-)
-
-const Content = ({course}) => (
-  <>
-    {course.parts.map(e => <Part key={e.id} part={e.name} exercises={e.exercises} />)}
-  </>
-)
-
-const Total = ({course}) => {
-  const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
-
-  return (
-    <p>
-      <strong>total of exercises {total}</strong>
-    </p>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course}/>
-    </div>
-  )
-}
-
-const Courses = ({courses}) => (
-  <>
-    {courses.map(course => <Course key={course.name} course={course}/>)}
-  </>
-)
+import Courses from './Courses'
 
 const App = () => {
   const courses = [
