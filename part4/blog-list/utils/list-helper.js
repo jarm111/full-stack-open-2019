0d490aa44f74f13fef74ceb16db630beac3798a6
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-08-15 13:56:41 +0300

    Add 4.4: apufunktioita ja yksikkötestejä, step2

--- part4/blog-list/utils/list-helper.js
+++ part4/blog-list/utils/list-helper.js
@@ -1,6 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => 1
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => sum + blog.likes
+
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
