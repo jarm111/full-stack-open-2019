226b4c9818eb5275c2a839baa730835f3e60c70f
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-08-15 14:05:27 +0300

    Move test data to it's own file

--- part4/blog-list/tests/dummy.test.js
+++ part4/blog-list/tests/dummy.test.js
@@ -1,8 +1,7 @@
 const listHelper = require('../utils/list-helper')
+const { emptyList } = require('./test-data')
 
 test('dummy return one', () => {
-  const blogs = []
-
-  const result = listHelper.dummy(blogs)
+  const result = listHelper.dummy(emptyList)
   expect(result).toBe(1)
 })
