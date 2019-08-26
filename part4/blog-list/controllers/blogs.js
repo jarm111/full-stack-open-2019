bf662d99c213097f0941e660c91eb867813ad325
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-08-26 13:02:15 +0300

    Add 4.19: blogilistan laajennus, step8

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,16 +1,33 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const config = require('../utils/config')
 
 blogsRouter.get('/', async (req, res) => {
   const blogs = await Blog.find({}).populate('user', { blogs: 0 })
   res.json(blogs)
 })
 
+const extractToken = req => {
+  const authHeader = req.get('authorization')
+  if (authHeader && authHeader.toLowerCase().startsWith('bearer ')) {
+    return authHeader.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (req, res, next) => {
+  const { title, author, url, likes } = req.body
+  const token = extractToken(req)
+  if (!token) return res.status(401).json({
+    error: 'token is missing'
+  })
+
   try {
-    const user = await User.findOne()
-    const { title, author, url, likes } = req.body
+    const decodedPayload = jwt.verify(token, config.SECRET)
+
+    const user = await User.findById(decodedPayload.id)
 
     const blog = new Blog({
       title,
@@ -19,6 +36,7 @@ blogsRouter.post('/', async (req, res, next) => {
       likes,
       user: user._id
     })
+
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -11,6 +11,10 @@ const errorHandler = (err, req, res, next) => {
 
   if (err.name === 'ValidationError') {
     return res.status(400).json({ error: err.message })
+  } else if (err.name === 'JsonWebTokenError') {
+    return res.status(401).json({
+      error: 'invalid token'
+    })
   }
 
   logger.error(err.message)
