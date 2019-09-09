0ba647bdf902f23aeb862e117edecf9a442bd541
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-09-09 16:25:09 +0300

    Add part5 bloglist-frontend starting point

---
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const baseUrl = '/api/blogs'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+export default { getAll }
