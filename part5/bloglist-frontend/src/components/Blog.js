42015001f11a67b05c26e1b62e86a21d864c2db4
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-09-16 11:01:23 +0300

    Add 5.6* blogilistan frontend, step6

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const showWhenVisible = { display: detailsVisible ? '' : 'none'}
+
+  const toggleVisiblity = () => {
+    setDetailsVisible(!detailsVisible)
+  }
+
+  return (
+    <div style={blogStyle} onClick={toggleVisiblity}>
+      <div>
+        {blog.title} {blog.author}
+      </div>
+      <div style={showWhenVisible}>
+        <a href={blog.url}>{blog.url}</a> <br />
+        {blog.likes} likes <button>like</button> <br />
+        added by {blog.user.name} <br />
+      </div>
+    </div>
+  )
+}
 
 export default Blog
