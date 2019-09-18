aad2482269231057fe1419dc4fca8b2b0626079d
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-09-18 13:20:04 +0300

    Add 5.13: blogilistan testit, step1

---
+++ part5/bloglist-frontend/src/setupTests.js
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom/extend-expect'
+
+const originalError = console.error
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Please upgrade to at least react-dom/.test(args[0])) {
+      return
+    }
+    originalError.call(console, ...args)
+  }
+})
+
+afterAll(() => {
+  console.error = originalError
+})
