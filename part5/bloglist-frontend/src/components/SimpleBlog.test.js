612586225e21b1dca3902ce15ba5156e59f8a5e5
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-09-18 13:58:31 +0300

    Add 5.14: blogilistan testit, step2

--- part5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ part5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import SimpleBlog from './SimpleBlog'
 
 test('renders content', () => {
@@ -15,4 +15,22 @@ test('renders content', () => {
   getByText(blog.title, options)
   getByText(blog.author, options)
   getByText(blog.likes.toString(), options)
+})
+
+test('click like button twice calls event handler equal times', async () => {
+  const blog = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    likes: 666
+  }
+
+  const mockHandler = jest.fn()
+
+  const { getByText } = render(<SimpleBlog blog={blog} onClick={mockHandler} />)
+
+  const button = getByText('like')
+  fireEvent.click(button)
+  fireEvent.click(button)
+
+  expect(mockHandler.mock.calls.length).toBe(2)
 })
