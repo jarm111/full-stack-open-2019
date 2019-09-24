a560381501f96b8a634e301df17277803cbd703e
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-09-24 14:26:47 +0300

    Add 5.21*: ultimate hooks

--- part5/custom-hooks/src/App.js
+++ part5/custom-hooks/src/App.js
@@ -19,10 +19,21 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    axios.get(baseUrl)
+      .then(res => {
+        setResources(res.data)
+    })
+  }, [baseUrl])
+  
 
   const create = (resource) => {
-    // ...
+    axios.post(baseUrl, resource)
+      .then(res => res.data)
+      .then(() => axios.get(baseUrl))
+      .then(res => {
+        setResources(res.data)
+    })
   }
 
   const service = {
