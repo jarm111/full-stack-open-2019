771adf630571d47a56dfd420cd953bac890ffaeb
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-11-22 13:27:20 +0200

    Add 8.2: kaikki kirjat ja kirjailijat

--- part8/library-server/index.js
+++ part8/library-server/index.js
@@ -85,10 +85,19 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
   type Query {
     hello: String!
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
@@ -96,7 +105,8 @@ const resolvers = {
   Query: {
     hello: () => { return 'world ' + new Date },
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
